9648c8b21eebbf60b975baf9b1163564d2d5e56d
Tbelkk <tylerbelkenyt@gmail.com>
2025-03-05 19:12:13 -0600

    Updated gui.py
    Changing from tkinter to customtkinter

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
-import tkinter as tk
-from tkinter import messagebox
+import customtkinter
 import json
 import os
 
@@ -30,59 +29,17 @@ def delete_app(name):
 def get_apps():
     return load_apps()
 
-class AppManagerGUI(tk.Tk):  # Inherit from tk.Frame instead of tk.Tk
+class Gui(customtkinter.CTk):
     def __init__(self):
-        super().__init__(self)  # Pass parent to tk.Frame
-        self.apps = load_apps()
+        super().__init__()
+        self.geometry("1000x500")
+
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
+        self.button.pack(padx=20, pady=20)
+
         
-        self.app_listbox = tk.Listbox(self)
-        self.app_listbox.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
-        self.update_app_listbox()
-
-        self.name_label = tk.Label(self, text="App Name:")
-        self.name_label.pack(pady=5)
-        self.name_entry = tk.Entry(self)
-        self.name_entry.pack(pady=5)
-
-        self.path_label = tk.Label(self, text="App Path:")
-        self.path_label.pack(pady=5)
-        self.path_entry = tk.Entry(self)
-        self.path_entry.pack(pady=5)
-
-        self.add_button = tk.Button(self, text="Add App", command=self.add_app)
-        self.add_button.pack(pady=10)
-
-        self.delete_button = tk.Button(self, text="Delete App", command=self.delete_app)
-        self.delete_button.pack(pady=10)
-
-    def update_app_listbox(self):
-        self.app_listbox.delete(0, tk.END)
-        for app in self.apps:
-            self.app_listbox.insert(tk.END, app)
-
-    def add_app(self):
-        name = self.name_entry.get()
-        path = self.path_entry.get()
-        if name and path:
-            add_app(name, path)
-            self.apps = load_apps()
-            self.update_app_listbox()
-            self.name_entry.delete(0, tk.END)
-            self.path_entry.delete(0, tk.END)
-        else:
-            messagebox.showerror("Input Error", "Please enter both name and path.")
-
-    def delete_app(self):
-        selected_app = self.app_listbox.curselection()
-        if selected_app:
-            app_name = self.app_listbox.get(selected_app[0])
-            delete_app(app_name)
-            self.apps = load_apps() 
-            self.update_app_listbox()
-        else:
-            messagebox.showerror("Selection Error", "Please select an app to delete.")
 
 
 if __name__ == "__main__":
-    app = AppManagerGUI()
-    app.mainloop()
+    gui = Gui()
+    gui.mainLoop()
